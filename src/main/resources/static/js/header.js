4e90614c07229cb422cd83ed6e53f35240c6c209
Artur <aginter11@ya.ru>
2020-10-31 18:11:11 +0500

    Фиксация верхнего блока

---
+++ src/main/resources/static/js/header.js
@@ -0,0 +1,19 @@
+$(function () {
+    $nav = $('.main-header');
+    $nav.css('width', $nav.outerWidth());
+    $window = $(window);
+    $h = $nav.offset().top;
+    $window.scroll(function () {
+        if ($window.scrollTop() > $h) {
+            $nav.addClass('fixed');
+        } else {
+            $nav.removeClass('fixed');
+        }
+    });
+});
+
+
+
+
+
+
