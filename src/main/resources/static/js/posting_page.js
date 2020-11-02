6f4b8538266a6f4da50c8ed640eae19a7de69e06
Artur <aginter11@ya.ru>
2020-11-02 17:31:58 +0500

    Реализация количества просмотровв объявлении

--- src/main/resources/static/js/posting_page.js
+++ src/main/resources/static/js/posting_page.js
@@ -106,5 +106,6 @@ async function getPostingInfo(id) {
     $("#contactNumber").append(`<br>${postingDto.contact}`);
     $("#address").append(`${postingDto.city}`);
     $("#description").append(`${postingDto.description}`);
+    $("#viewNumber").append(`${postingDto.viewNumber}`);
 
 }
