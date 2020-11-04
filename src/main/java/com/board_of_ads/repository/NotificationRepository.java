81efa24c1b65e4eb4dbf5d38cf5820ea652abd8c
timursharipov <timvlpn@gmail.com>
2020-11-04 20:22:45 +0300

    updating...

--- src/main/java/com/board_of_ads/repository/NotificationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.board_of_ads.repository;
-
-import com.board_of_ads.models.Notification;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface NotificationRepository extends JpaRepository<Notification, Long> {
-}
--- src/main/java/com/board_of_ads/service/interfaces/OAuth2Service.java
+++
@@ -1,9 +0,0 @@
-package com.board_of_ads.service.interfaces;
-
-import com.board_of_ads.models.User;
-
-public interface OAuth2Service {
-
-    void auth();
-    void setAuthenticated(User user);
-}
