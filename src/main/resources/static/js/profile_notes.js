7bbcc133a818a3b3101ab3c83b663e9899ab313d
asamara56 <a_g93@mail.ru>
2020-11-06 23:11:03 +0300

    add_no-notification-image

--- src/main/resources/static/js/profile_notes.js
+++ src/main/resources/static/js/profile_notes.js
@@ -1,58 +1,38 @@
-let usersProfilePostsContent = $('#profile-posts-content');
-
 let allNotesCount = 0;
 let newNotesCount = 0;
 let readNotesCount = 0;
-
-const usersProfileNewNotifications = $("#tbody_new");
-const usersProfileReadNotifications = $("#tbody_read");
-const userId = document.getElementById('profile-user-id').textContent;
-const requestURL = '/api/notification/user/';
-
-const valueNoteBell = document.getElementsByClassName("note_bell");
+const url = '/api/notification/';
 
 $(document).ready(function () {
-    getUserAllNotifications();
+    return getUserAllNotifications();
 });
 
-const httpHeader = {
-    fetch: async function (url, options = {}) {
-        const response = await fetch(url, {
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            ...options,
-        });
-        return response.json();
-    }
-}
+async function getUserAllNotifications() {
+    document.getElementById('tbody_new').innerHTML = "";
+    document.getElementById('tbody_read').innerHTML = "";
 
-function getUserAllNotifications() {
-    userHasAnyNotes();
-    profileNotesService.findNotificationsByUserId(userId).then((response => {
-        response.data.map(note => {
+    await fetch(url, {method : "GET"})
+        .then(response => response.json())
+        .then((response => {
+        if (response.data !== undefined) {
+            response.data.map(note => {
 
-            for (let o in note) {
                 let messageTitle = document.createTextNode(note.messageTitle);
                 let messageBody = document.createTextNode(note.messageBody);
                 let clickAction = document.createTextNode(note.clickAction);
                 let sentTime = document.createTextNode(note.sentTime);
 
-                let acceptNumber = note.acceptNumber;
                 let notificationId = note.notificationId;
-                let userIdFromRequest = note.userId;
-                let status  = note.status;
                 let urgentLevel = note.urgentLevel;
 
                 let trNoteBlock = document.createElement('tr');
-                trNoteBlock.setAttribute('id', 'notification-block-' + notificationId);
+                trNoteBlock.setAttribute('id', 'notificationBlock' + notificationId);
 
                 let li = document.createElement('li');
                 li.setAttribute('class', 'dropdown-divider');
 
                 let trHead = document.createElement('tr');
-                let trBody= document.createElement('tr');
+                let trBody = document.createElement('tr');
                 let trInfo = document.createElement('tr');
                 let trFooter = document.createElement('tr');
 
@@ -90,16 +70,14 @@ function getUserAllNotifications() {
                         levelSpan.innerText = 'Важное!';
                         tdNewBadge.appendChild(levelSpan);
                     }
-                    let readButtonVsId = $("<button type='button' class='btn-sm btn-outline-info' " +
-                        "onclick='onChangeButt(value)' " +
-                        "id='readButton' name='id' value='1'></button>").text('Прочел').appendTo(buttonDiv);
-                    readButtonVsId.attr('value', notificationId);
-                }
-                else {
-                    let deleteButtonVsId = $("<button type='button' class='btn-sm btn-outline-info' " +
-                        "onclick='onDeleteButt(value)'   " +
-                        "id='deleteButton' name='id' value='1'></button>").text('Удали').appendTo(buttonDiv);
-                    deleteButtonVsId.attr('value', notificationId);
+                    $("<button type='button' class='btn-sm btn-success' " +
+                        "onclick='onChangeButt("+notificationId+")' id='button"+notificationId+"'></button>")
+                        .text('Прочитано').appendTo(buttonDiv);
+
+                } else {
+                    $("<button type='button' class='btn-sm btn-danger' " +
+                        "onclick='onDeleteButt("+notificationId+")' id='button"+notificationId+"'></button>")
+                        .text('Удалить').appendTo(buttonDiv);
                 }
 
                 tdTitle.appendChild(messageTitle);
@@ -114,92 +92,77 @@ function getUserAllNotifications() {
                 trNoteBlock.appendChild(li);
 
                 if (note.status === 'newSent') {
-                    usersProfileNewNotifications.append(trNoteBlock);
+                    $("#tbody_new").append(trNoteBlock);
                 } else {
-                    usersProfileReadNotifications.append(trNoteBlock);
+                    $("#tbody_read").append(trNoteBlock);
                 }
-                break;
-            }
+            });
+        }}));
+
+    await fetch(url + 'count-map', {method: 'GET'})
+        .then(response => response.json())
+        .then(responseData => {
+            allNotesCount = responseData.data[0];
+            newNotesCount = responseData.data[1];
+            readNotesCount = responseData.data[2];
+
+            document.getElementById('new-notes-tab').innerText = 'Новые ' + ' - ' + newNotesCount;
+            document.getElementById('read-notes-tab').innerText = 'Прочитанные ' + ' - ' + readNotesCount;
+            return true;
         });
-    }));
-}
 
-const profileNotesService = {
-    findNotificationsByUserId: async (id) => {
-        return await httpHeader.fetch(requestURL + id);
+    if (allNotesCount === 0 ) {
+        return document.getElementById('allNotification').innerHTML = '' +
+            '<div class="text-center align-middle">' +
+            '   <img src="images/ikonka-uvedomlenij.png" class="align-self-center ">' +
+            '   <div class="text-muted">' +
+            '       <div>Нет уведомлений</div>' +
+            '       <div>На этой странице будут собраны</div> ' +
+            '       <div>все уведомления</div>' +
+            '   </div> ' +
+            '</div>';
+    } else {
+        if (newNotesCount === 0 ) {
+            return document.getElementById('tbody_new').innerHTML = '' +
+                '<div class="text-center align-middle">' +
+                '   <img src="images/ikonka-uvedomlenij.png" class="align-self-center ">' +
+                '   <div class="text-muted">' +
+                '       <div>Нет новых уведомлений</div>' +
+                '       <div>На этой странице будут собраны </div>' +
+                '       <div>новые уведомления</div>' +
+                '   </div> ' +
+                '</div>';
+        }
+
+        if (readNotesCount === 0 ) {
+            return document.getElementById('tbody_read').innerHTML = '' +
+                '<div class="text-center align-middle">' +
+                '   <img src="images/ikonka-uvedomlenij.png" class="align-self-center ">' +
+                '   <div class="text-muted">' +
+                '       <div>Нет прочитанных уведомлений</div>' +
+                '       <div>На этой странице будут собраны </div>' +
+                '       <div>прочитанные уведомления</div>' +
+                '   </div> ' +
+                '</div>';
+
+        }
     }
 }
 
-function userHasAnyNotes() {
-    getResData(userId).then((responseData) => {
-        let ncm = responseData.data;
-        allNotesCount = ncm[0];
-        newNotesCount = ncm[1];
-        readNotesCount = ncm[2];
-        console.log('Checking count ....... ');
-        document.getElementById('new-notes-tab').innerText = 'Новые ' + ' - ' + newNotesCount;
-        document.getElementById('read-notes-tab').innerText = 'Прочитанные ' + ' - ' + readNotesCount;
-        if (newNotesCount === 0 ) {valueNoteBell.style.backgroundColor = 'black'; }
-    })
+async function onChangeButt(noteId) {
 
-function getResData() {
-        return fetch(requestURL + userId + '/count-map', {method: 'GET'})
-            .then(response => response.json())
-            .then(data => {
-                return data;
-            })
-    }
-}
-
-function onChangeButt(noteId) {
-    changeStatusToRead(userId, noteId);
-    userHasAnyNotes();
-}
-
-function changeStatusToRead(userId, noteId) {
-    let url = requestURL + userId + '/' + noteId + '/change-status-to-read';
-    sendRequest('PATCH', url)
+    return await fetch(url + noteId, {method: 'PATCH'})
         .then(() => {
-                console.log('User ' + userId + ' note ' + noteId +' is changed')
-                let deleteBlock = "#notification-block-" + noteId;
-                $(deleteBlock).remove();
-            }
-        )
-        .catch(err => console.log(err))
-
-    async function sendRequest(method, url) {
-        fetch(url, {
-            method: method
+            return getUserAllNotifications();
         })
-            .then((response) => {
-                return response;
-            })
-    }
+        .catch(err => console.log(err));
 }
 
-function onDeleteButt(noteId) {
-    deleteNoteFromUserFunction(userId, noteId);
-    userHasAnyNotes();
-}
+async function onDeleteButt(noteId) {
 
-function deleteNoteFromUserFunction(userId, noteId) {
-    let url = requestURL + userId + '/' + noteId + '/delete';
-    sendRequest('DELETE', url)
+    return await fetch(url + noteId, {method: 'DELETE'})
         .then(() => {
-                console.log('User ' + userId + ' note ' + noteId +' is deleted')
-                let deleteBlock = "#notification-block-" + noteId;
-                $(deleteBlock).remove();
-
-            }
-        )
-        .catch(err => console.log(err))
-
-    async function sendRequest(method, url) {
-        fetch(url, {
-            method: method
+            return getUserAllNotifications();
         })
-            .then((response) => {
-                return response;
-            })
-    }
+        .catch(err => console.log(err));
 }
