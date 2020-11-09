d317bce224e06a6e1cd0a81471edf3375e9cd3dc
asamara56 <a_g93@mail.ru>
2020-11-09 11:03:12 +0300

    refactor_3

--- src/main/resources/static/js/newpost.js
+++ src/main/resources/static/js/newpost.js
@@ -1,66 +1,38 @@
-let displayLayer = 1;
+let displayLayer = 0;
 
 $(document).ready(function () {
-    getParentCategories().then();
+    getCategoryColumn(0, 0).then();
 })
 
-async function getParentCategories() {
+async function getCategoryColumn(parentID, selectLayer) {
+    let categories = await getCategories(await getURL(parentID));
 
-    let categories = await getCategories('/api/category/allParentCategory');
-
-    for (let i = 0; i < categories.length; i++) {
-        let category = categories[i];
-        document.getElementById('cascadeTableColumn1').innerHTML +=
-            `<div class="category-table-button inactive-category-table-button layer1" 
-                    id="categoryTableButton${category.id}" onclick="clickOnCategoryButton(this, ${category.id}, 1)" 
-                    onMouseOver="this.style.backgroundColor='DeepSkyBlue', this.style.color='white'" 
-                    onMouseOut="this.style.backgroundColor='white', this.style.color='#555'">
-                ${category.name}
-            </div>`
-    }
-    return true;
-}
-
-async function getChildCategory(parentID, selectLayer) {
-
-    let categories = await getCategories('/api/category/allChildCategories/' + parentID);
-
-    if (selectLayer <= displayLayer) {
-        for (let i = selectLayer+1; i <= displayLayer; i++) {
-            document.getElementById('cascadeTableColumn' + i).remove();
-        }
-        displayLayer = selectLayer;
-    }
+    await cleanCategoryColumns(selectLayer);
 
     if (categories.length !== 0) {
-        let parent = await getCategories('/api/category/' + parentID);
+        displayLayer++;
+        let temp = document.getElementById('cascadeTableButton_' + parentID);
+        let parentName = (temp === null) ? 'Категория' : temp.textContent;
 
         document.getElementById('cascadeTableContainer').innerHTML +=
-            '<div id="cascadeTableColumn' + categories[0].layer + '" class="cascade-table-column">' +
-            '   <div class="category-table-title">'+parent.name+'</div>' +
+            '<div id="cascadeTableColumn' + displayLayer + '" class="cascade-table-column">' +
+            '   <div class="category-table-title">'+parentName+'</div>' +
             '</div>';
 
         for (let i = 0; i < categories.length; i++) {
             let category = categories[i];
-            let id = 'cascadeTableElement_' + (category.frontName === null ? category.id : category.frontName);
-            document.getElementById('cascadeTableColumn' + category.layer).innerHTML +=
-                `<div class="category-table-button inactive-category-table-button layer${category.layer}" 
-                        id="${id}" onclick="clickOnCategoryButton(this, ${category.id}, ${category.layer})"
-                        onMouseOver="this.style.backgroundColor='#0af', this.style.color='white'" 
-                        onMouseOut="this.style.backgroundColor='white', this.style.color='#555'">
+            let id = 'cascadeTableButton_' + (category.frontName === null ? category.id : category.frontName);
+            document.getElementById('cascadeTableColumn' + displayLayer).innerHTML +=
+                `<div id="${id}" onMouseOver="hoverOnMouseOver()" 
+                        class="category-table-button inactive-category-table-button layer${displayLayer}"  
+                        onclick="clickOnCategoryButton(this, ${category.id}, ${displayLayer})">
                 ${category.name}
             </div>`
         }
-        displayLayer++;
         return true;
     } else {
-        $('#pathCategory').show();
-        $('#visibleElement1').hide();
-        $('#visibleElement2').show();
-        $('#visibleElement3').show();
-        return true;
+        return await changeVisible();
     }
-    return false;
 }
 
 async function clickOnCategoryButton(o, id, selectLayer) {
@@ -68,17 +40,10 @@ async function clickOnCategoryButton(o, id, selectLayer) {
     $(className)
         .removeClass("active-category-table-button")
         .addClass("inactive-category-table-button").css("background-color", "#fff");
-    $(this).removeClass("inactive-category-table-button")
+    $(o).removeClass("inactive-category-table-button")
         .addClass("active-category-table-button").css("background-color", "#0af", "color", "#fff");
-    /*const items = document.querySelectorAll('.layer' + selectLayer);
-    Array.from(items).forEach(item => {
-        item.classList.remove('active-category-table-button');
-        item.classList.add('inactive-category-table-button');
-    })
 
-    $(object).addClass("active-category-table-button").css("background-color", "DeepSkyBlue", "color", "#ffffff");*/
-
-    return await getChildCategory(id, selectLayer);
+    return await getCategoryColumn(id, selectLayer);
 }
 
 async function getCategories(url) {
@@ -92,20 +57,41 @@ async function getCategories(url) {
     return (await response.json()).data;
 }
 
-/*function hoverOnCategoryButton() {
+function hoverOnMouseOver() {
     $(".category-table-button").hover(
         function () {
-            if (this.classList.contains("inactive-category-table-button")) {
-                $(this).css("background-color", "#a5eaf8");
+            if (!this.classList.contains("active-category-table-button")) {
+                $(this).css("background-color", "#0af");
+            }
+        }, function () {
+            if (!this.classList.contains("active-category-table-button")) {
+                $(this).css("background-color", "#fff");
             }
         });
-    $(".category-table-button").hover(
-        function () {
-            if (this.classList.contains("active-category-table-button")) {
-                $(this).css("background-color", "#ffffff");
-            }
-        });
-}*/
+}
+
+async function getURL(parentID) {
+    return (displayLayer === 0
+        ? '/api/category/allParentCategory'
+        : '/api/category/allChildCategories/' + parentID);
+}
+
+async function cleanCategoryColumns(selectLayer) {
+    if (selectLayer <= displayLayer) {
+        for (let i = selectLayer+1; i <= displayLayer; i++) {
+            document.getElementById('cascadeTableColumn' + i).remove();
+        }
+        displayLayer = selectLayer;
+    }
+}
+
+async function changeVisible() {
+    $('#pathCategory').show();
+    $('#visibleElement1').hide();
+    $('#visibleElement2').show();
+    $('#visibleElement3').show();
+    return true;
+}
 
 $('#pathCategoryButton').on('click', function () {
     $('#pathCategory').hide();
