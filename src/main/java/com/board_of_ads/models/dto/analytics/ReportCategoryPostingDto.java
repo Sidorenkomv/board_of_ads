5956d0516bd8e7a19d4bd7a769db7083d7e2131b
Artur <aginter11@ya.ru>
2020-11-27 12:59:45 +0500

    Аналитика по категориям

--- src/main/java/com/board_of_ads/models/dto/analytics/ReportCategoryPostingDto.java
+++ src/main/java/com/board_of_ads/models/dto/analytics/ReportCategoryPostingDto.java
@@ -1,13 +1,12 @@
 package com.board_of_ads.models.dto.analytics;
 
 import com.board_of_ads.models.Category;
-import com.board_of_ads.service.interfaces.Combine;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
-public class ReportCategoryPostingDto implements Combine<ReportCategoryPostingDto> {
+public class ReportCategoryPostingDto {
     private String category;
     private Long categoryId;
     private Category parentCategory;
@@ -20,27 +19,5 @@ public class ReportCategoryPostingDto implements Combine<ReportCategoryPostingDt
         this.postsCount = postsCount;
         this.activePostsCount = activePostsCount;
         this.archivePostsCount = archivePostsCount;
-    }
-
-
-    @Override
-    public ReportCategoryPostingDto mergeParent(ReportCategoryPostingDto o1, ReportCategoryPostingDto o2) {
-
-        if (o1.getParentCategory().getName().equals(o2.getParentCategory().getName())) {
-            return new ReportCategoryPostingDto(o1.getParentCategory().getName(), o1.getPostsCount() + o2.getPostsCount(),
-                    o1.getActivePostsCount() + o2.getActivePostsCount(),
-                    o1.getArchivePostsCount() + o2.getArchivePostsCount());
-        }
-        return null;
-    }
-
-    public ReportCategoryPostingDto mergeGrandParent(ReportCategoryPostingDto obj) {
-
-        if (this.getParentCategory().getCategory().getName().equals(obj.getParentCategory().getCategory().getName())) {
-            return new ReportCategoryPostingDto(this.getParentCategory().getCategory().getName(), this.getPostsCount() + obj.getPostsCount(),
-                    this.getActivePostsCount() + obj.getActivePostsCount(),
-                    this.getArchivePostsCount() + obj.getArchivePostsCount());
-        }
-        return null;
     }
 }
--- src/main/java/com/board_of_ads/service/interfaces/Combine.java
+++
@@ -1,6 +0,0 @@
-package com.board_of_ads.service.interfaces;
-
-public interface Combine<T> {
-
-    public T mergeParent(T o1, T o2);
-}
